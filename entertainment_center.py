dac2e167d40a7048afd0168b91f3b4e64ea59d7d
pradeepthi15pa1a04g9 <38848947+pradeepthi15pa1a04g9@users.noreply.github.com>
2018-05-22 10:30:04 +0530

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,19 +1,22 @@
-#!/usr/bin/env python
-print("content-type:text/html \n")
 import media
 import fresh_tomatoes
 
-nemo= media.Movie("nemo","finding nemo","http://www.weaverstreetmarket.coop/wp-content/uploads/2016/06/movie-poster-nemo.jpeg",
-                    "https://www.youtube.com/embed/gfgeIZyrIM0")
-sw=media.Movie("snowwhite","about a girl","https://image.tmdb.org/t/p/w185/wbVGRBYPRRahIZNGXY9TfHDUSc2.jpg"
-                ,"https://www.youtube.com/embed/IfePzXxIuvc")
-tj=media.Movie("tom&jerry","Tom&Jerry","https://upload.wikimedia.org/wikipedia/en/b/b4/Tom_and_Jerry_-_The_Movie_Poster.png","https://www.youtube.com/embed/WBYdp2sOut0")
-moana=media.Movie("moana","saving island","https://cdn.disneymovieclub.go.com/static/Catalog/Acquisition_Agnostic_191x269/Moana_Acq.jpg"
-                 ,"https://www.youtube.com/embed/LKFuXETZUsI")
-up=media.Movie("up","adventures","https://www.cardiffboxoffice.com/asset/Event/11505/up.jpg","https://www.youtube.com/embed/ORFWdXl_zJ4")
-cinderella=media.Movie("cinderella","cinderella","https://www.columbiaassociation.org/wp-content/uploads/2016/03/ShowImage-id=10603&t=635689308124800000.jpeg"
-                                ,"https://www.youtube.com/embed/eq-bqSvx0R8")
-                              
-
-movies = [nemo,sw,tj,moana,up,cinderella]
+# New movie object.
+nemo = media.Movie("nemo", "finding nemo", "https://bit.ly/2kcXyGQ",
+                   "https://www.youtube.com/embed/gfgeIZyrIM0")
+# New movie object.
+snowwhite = media.Movie("snowwhite", "about a girl", "https://bit.ly/2KKiPTw",
+                        "https://www.youtube.com/embed/WBYdp2sOut0")
+# New movie object.
+moana = media.Movie("moana", "saving island", "https://bit.ly/2IU4RBt",
+                    "https://www.youtube.com/embed/LKFuXETZUsI")
+# New movie object.
+up = media.Movie("up", "adventures", "https://bit.ly/2s1R957",
+                 "https://www.youtube.com/embed/ORFWdXl_zJ4")
+# New movie object.
+cinderella = media.Movie("cinderella", "cinderella", "https://bit.ly/2KKYrl8",
+                         "https://www.youtube.com/embed/eq-bqSvx0R8")
+# Array with all movies
+movies = [nemo, snowwhite, tomandjerry, moana, up, cinderella]
+# Call the function open movie page from the file fresh_tomatoes
 fresh_tomatoes.open_movies_page(movies)
