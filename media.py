b416afbf424bd90ddb45e116f6e6ef88d8a0ece9
pradeepthi15pa1a04g9 <38848947+pradeepthi15pa1a04g9@users.noreply.github.com>
2018-05-22 10:47:12 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,18 +1,20 @@
-#!/usr/bin/env python
-print("content-type:text/html \n")
 import webbrowser
 
 
-
 class Movie():
-    VALID_RATINGS=["EXCELLENT","GOOD","BAD","AVERAGE"]
+        VALID_RATINGS = ["EXCELLENT", "GOOD", "BAD", "AVERAGE"]
 
-    def __init__(self,movie_title,movie_storyline,poster_image,trailer_youtube):
-        self.title = movie_title
-        self.storyline = movie_storyline
-        self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube
+        """This class contain the srtructure of the object movie"""
+        # Constructor of the class
+        def __init__(self, movie_title, movie_storyline,
+                     poster_image, trailer_youtube):
+            self.title = movie_title
+            self.storyline = movie_storyline
+            self.poster_image_url = poster_image
+            self.trailer_youtube_url = trailer_youtube
+        # Method to show the trailer of a movie in the browser
 
-    def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
+        def show_trailer(self):
+            webbrowser.open(self.trailer_youtube_url)
+
             
